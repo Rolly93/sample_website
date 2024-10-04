ba9507be19e414cb3ad90c00a019cd5dabe40efe
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2024-10-04 22:33:47 +0000

    post proyect from udemy

---
+++ public/views/js/index.js
@@ -0,0 +1,10 @@
+const button = document.querySlector('button [type ="submit"]');
+const input = document.querySelecetor('input [name="user-input']");
+
+const userContent = [];
+button.addEventListener('click',()=> {
+const username = input.value;
+user.Content.push(
+for insert in userContent 
+
+documento.querySelector('div [class = "item4"]").textContent = username
